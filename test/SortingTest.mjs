4148b18ba62ddc5269c72f27e7815f87c376a068
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-11-02 11:42:46 +0800

    Revised testing structure to incorporate algorithms that may be added in the future.

--- test/SortingTest.mjs
+++ test/SortingTest.mjs
@@ -10,13 +10,19 @@ const test_data = [
   {args: [2, 1, 11, 12, 3, 5, 13, 6, 4, 7, 9, 8, 10], expected: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]},
 ]
 
+const Sorters = [
+  {name: "HeapSort", instance: new HeapSort()},
+]
+
 describe('Sorting Algorithm', function () {
-  test_data.forEach(({args, expected}) => {
-    it(`correctly sort the given array: ${args} using HeapSort`, function () {
-      const sortingInstance = new HeapSort();
-      sortingInstance.Sort(args);
-      assert.deepEqual(args, expected);
+  Sorters.forEach(({name, instance}) => {
+    test_data.forEach(({args, expected}) => {
+      it(`correctly sort the given array: ${args} using ${name}`, function () {
+        const sortingInstance = instance;
+        sortingInstance.Sort(args);
+        assert.deepEqual(args, expected);
+      });
     });
-  });
+  })
 });
 
